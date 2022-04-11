3638d238b889c1562f810c247626df1938690ff8
Linda Marko <lindamarko83@gmail.com>
2022-04-11 10:22:02 +0200

    Add pagination

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -20,7 +20,7 @@ exports.getAllTours = async (req, res) => {
       const sortBy = req.query.sort.split(',').join(' ');
       query = query.sort(sortBy);
     } else {
-      query = query.sort('-createdAt');
+      query = query.sort('_id');
     }
 
     //3) FIELD LIMITING
@@ -29,6 +29,20 @@ exports.getAllTours = async (req, res) => {
       query = query.select(fields);
     } else {
       query = query.select('-__v'); //exclude __v
+    }
+
+    //4) PAGINATION
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+    console.log(page, limit, skip);
+
+    //page=3&limit10, 1-10, page 1, 11-20, page 2, 21-30, page 3
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist');
     }
 
     //EXECUTE QUERY
